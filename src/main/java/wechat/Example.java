b0ae3e043f89aff957f8df50596d64c81bf83d03
tianjian <1731857742@qq.com>
2017-10-30 16:40:08 +0800

    Initial commit

---
+++ src/main/java/wechat/Example.java
@@ -0,0 +1,17 @@
+package wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:Administrator@gtmap.cn">Administrator</a>
+ * @version 1.0, 2017/2/21
+ * @description
+ */
+@SpringBootApplication
+public class Example
+{
+    public static void main(String[] args){
+        SpringApplication.run(Example.class);
+    }
+}
